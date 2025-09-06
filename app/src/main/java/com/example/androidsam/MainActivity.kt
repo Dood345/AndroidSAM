0fb9db121e51b8b77f610eb0a4d7f2010d520b33
dripleyb <dripleyb@iastate.edu>
2025-09-06 11:11:15 -0500

    removed internet permissions, added pitch, speed, mouth, throat sliders

--- app/src/main/java/com/example/androidsam/MainActivity.kt
+++ app/src/main/java/com/example/androidsam/MainActivity.kt
@@ -12,6 +12,8 @@ import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +26,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var speakButton: Button
     private lateinit var webView: WebView
     private lateinit var progressBar: ProgressBar
+    private lateinit var pitchSeekBar: SeekBar
+    private lateinit var speedSeekBar: SeekBar
+    private lateinit var mouthSeekBar: SeekBar
+    private lateinit var throatSeekBar: SeekBar
+    private lateinit var pitchValue: TextView
+    private lateinit var speedValue: TextView
+    private lateinit var mouthValue: TextView
+    private lateinit var throatValue: TextView
 
     companion object {
         private const val JAVASCRIPT_LOG_TAG = "SAM_JS_LOG"
@@ -43,6 +53,16 @@ class MainActivity : AppCompatActivity() {
         speakButton = findViewById(R.id.speakButton)
         webView = findViewById(R.id.webView)
         progressBar = findViewById(R.id.progressBar)
+        pitchSeekBar = findViewById(R.id.pitchSeekBar)
+        speedSeekBar = findViewById(R.id.speedSeekBar)
+        mouthSeekBar = findViewById(R.id.mouthSeekBar)
+        throatSeekBar = findViewById(R.id.throatSeekBar)
+        pitchValue = findViewById(R.id.pitchValue)
+        speedValue = findViewById(R.id.speedValue)
+        mouthValue = findViewById(R.id.mouthValue)
+        throatValue = findViewById(R.id.throatValue)
+
+        setupSeekBarListeners()
 
         // Configure WebView
         webView.settings.javaScriptEnabled = true
@@ -84,13 +104,51 @@ class MainActivity : AppCompatActivity() {
                 Log.d(JAVASCRIPT_LOG_TAG, "Speak button clicked with text: $textToSpeak")
                 // Basic text cleaning (replace newlines and escape quotes for JS)
                 val cleanedText = textToSpeak.replace("\n", " ").replace("'", "\\'").replace("\"", "\\\"")
-                val script = "javascript:speakText('$cleanedText')"
+                val pitch = pitchSeekBar.progress
+                val speed = speedSeekBar.progress
+                val mouth = mouthSeekBar.progress
+                val throat = throatSeekBar.progress
+                val script = "javascript:speakText('$cleanedText', $pitch, $speed, $mouth, $throat)"
                 Log.d(JAVASCRIPT_LOG_TAG, "Evaluating script: $script")
                 webView.evaluateJavascript(script, null)
             } else {
                 Toast.makeText(this, "Please enter some text", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun setupSeekBarListeners() {
+        pitchSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                pitchValue.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        speedSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                speedValue.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        mouthSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                mouthValue.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        throatSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                throatValue.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 
     inner class SAMInterface {
